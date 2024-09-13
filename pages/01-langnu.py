3fbae1ea11add711d50c033b6dd31069e80c3e70
Quang Huy <kaheetonaa@gmail.com>
2024-09-13 15:18:29 +0200

    update webmap

--- pages/01-langnu.py
+++ pages/01-langnu.py
@@ -8,22 +8,26 @@ st.title("Mapping lang Nu")
 
 m = leafmap.Map(height=600, center=[22.25, 104.5], zoom=12)
 
-m.add_basemap("HYBRID")
+m.add_basemap("SATELLITE")
 
 in_geojson = "https://github.com/kaheetonaa/Yagimapping/raw/main/Case01-LangNu/data/langnu-googleopenbuildings.geojson"
-dataframe=gpd.read_file(in_geojson)
+df=gpd.read_file(in_geojson)
+layer_style=dict({'No':'white','Non detected':'black','Yes':'red'})
 
-m.add_data(dataframe, layer_name="Cong trinh",categories=['No', 'Yes',None],colors=['white', 'red','gray'],column = "Destroy", legend_title='Cong trinh', labels=['Khong anh huong','Huy hoai','Chua xac dinh'],
-        style = {
+df['color']= df.replace({"Destroy": layer_style})['Destroy']
+
+
+m.add_data(df, layer_name="Cong trinh",categories=['No','Non detected','Yes'],colors=['white', 'black','red'],column = "Destroy", legend_title='Cong trinh', labels=['Khong anh huong','Chua xac dinh','Huy hoai'],
+        style_function = lambda x:({
             "stroke": True,
-            "color": "gray",
-            "weight": .5,
-            "opacity": .5,
-            "fill": True,               
-            "fillOpacity": 0.5,           
-        })
+            "color": "white",
+            "weight": 1,
+            "opacity": 1,             
+            "fillOpacity": 0.5,
+            "fillColor": x['properties']['color']         
+        }))
 
-url = "HYBRID"
+url = "SATELLITE"
 url_2= 'https://github.com/kaheetonaa/Yagimapping/raw/main/Case01-LangNu/data/lang-nu-drone-imagery-2.tif'
 #104,52751/22,15154
 
